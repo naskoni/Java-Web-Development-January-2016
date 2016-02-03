c26aadf98b736c252096d496bb4e034a5174f872
Atanas Atanasov <naskoni@gmail.com>
2016-02-03 21:47:30 +0200

    fix

--- Homework-ContainersFiltersSessions/src/bg/jwd/filters/SecurityFilter.java
+++ Homework-ContainersFiltersSessions/src/bg/jwd/filters/SecurityFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,8 +16,12 @@ import javax.servlet.http.HttpSession;
 @WebFilter("/*")
 public class SecurityFilter implements Filter {
 
+	private static final String USER_ATTR = "username";
+	private static final String HOME_PAGE = "/Homework-ContainersFiltersSessions/pages/HomePage.jsp";
+	private static final String LOGIN_PAGE = "/Homework-ContainersFiltersSessions/pages/LoginForm.jsp";
+	
 	private final String user = "Pesho";	
-	private final String password = "123";
+	private final String password = "123";	
 	
 	@Override
 	public void destroy() {
@@ -31,32 +34,28 @@ public class SecurityFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;			
-		
+					
 		String requestUsername = req.getParameter("user");
 		String requestPassword = req.getParameter("pwd");
 			
 		if (user.equals(requestUsername) && password.equals(requestPassword)) {			
-			request.getSession().setAttribute("username", requestUsername);
-			request.getSession().setMaxInactiveInterval(60);
+			request.getSession().setAttribute(USER_ATTR, requestUsername);
+			request.getSession().setMaxInactiveInterval(60);				
 			
-			Cookie cookie = new Cookie("user", requestUsername);
-			cookie.setMaxAge(60);			
-			response.addCookie(cookie);
-			
-			response.sendRedirect("/Homework-ContainersFiltersSessions/pages/HomePage.jsp");
+			response.sendRedirect(HOME_PAGE);
 		} else {			
-			if (request.getSession().getAttribute("username") != null) {				;
+			if (request.getSession().getAttribute(USER_ATTR) != null) {				;
 				chain.doFilter(req, res);
 			} else {
 				HttpSession session = request.getSession(false);
 				String loginURL = request.getContextPath() + "/pages/LoginForm.jsp";
 				
 				boolean isLogginRequest = loginURL.equals(request.getRequestURI());
-				boolean loggedIn = session != null && session.getAttribute("username") != null;
+				boolean loggedIn = session != null && session.getAttribute(USER_ATTR) != null;
 				if (loggedIn || isLogginRequest) {
 					chain.doFilter(req, res);			
 				} else {
-					response.sendRedirect("/Homework-ContainersFiltersSessions/pages/LoginForm.jsp");
+					response.sendRedirect(LOGIN_PAGE);
 				}	
 			}
 		}		
@@ -67,5 +66,4 @@ public class SecurityFilter implements Filter {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
